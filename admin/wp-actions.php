1f9f09eccc414ef3d60df0af11daee57cafeab76
Edwin Xico <contact@edwinxico.com>
2022-10-22 13:23:26 -0600

    Corregir url de trakcing guide - agregar template de ftp

--- admin/wp-actions.php
+++ admin/wp-actions.php
@@ -265,7 +265,7 @@ function dl_add_caex_invoice_to_order_details( $order ) {
 		<div class="d-flex flex-wrap order-info order-invoice m-b-xl m-t-xs p-t-lg">
 			<div class="order-item">
 				<strong><?= __('Tracking ID', 'wp-caex-woocommerce') ?></strong>
-				<mark class="font-weight-bold order-total"><a href="https://www.cargoexpreso.com/tracking/?guia==<?= $caex_dte['NumeroGuia'] ?>" target="_blank"><?= $caex_dte['NumeroGuia'] ?></a></mark>
+				<mark class="font-weight-bold order-total"><a href="https://www.cargoexpreso.com/tracking/?guia=<?= $caex_dte['NumeroGuia'] ?>" target="_blank"><?= $caex_dte['NumeroGuia'] ?></a></mark>
 			</div>
 		</div>
 		<?php
